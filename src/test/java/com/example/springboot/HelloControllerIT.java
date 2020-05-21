47a61be99107d2da9ff2508812b2633e2eaff4c1
Christopher Drew <christopher.drew1@nbcuni.com>
2020-05-20 23:46:39 -0400

    test: fix `Hello` integration test

--- src/test/java/com/example/springboot/HelloControllerIT.java
+++ src/test/java/com/example/springboot/HelloControllerIT.java
@@ -33,6 +33,6 @@ public class HelloControllerIT {
     public void getHello() throws Exception {
         ResponseEntity<String> response = template.getForEntity(base.toString(),
                 String.class);
-        assertThat(response.getBody()).isEqualTo("Helloooooo from a simple Spring Boot API!");
+        assertThat(response.getBody()).isEqualTo("Hello from a simple Spring Boot API!");
     }
 }
